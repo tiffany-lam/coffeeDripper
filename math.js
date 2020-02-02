36c652199c27a88470ebbe462046bd4adefc97c0
katrina <44593453+whaletrina@users.noreply.github.com>
2020-02-01 23:29:21 -0800

    coffee cups animation screen fill

--- math.js
+++ math.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $("submitName").click(function(){
-        var hours = $("#numberField").val();
+        var hours = $("#hoursAwake").val();
         function coffeeCalc(hour){
-            coffeeMg = hour // 5
-            cups = coffeeMg // 95
+            coffeeMg = (hour / 5) * 50
+            cups = coffeeMg / 95
             console.log(cups)    
-    }
-    coffeeCalc(hours)
+        }
+        coffeeCalc(hours)
     })
 })
